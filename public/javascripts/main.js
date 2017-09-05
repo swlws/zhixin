c32b94509d7f3fe66f4e3c1827286d43fb1ddb1b
swl979623074 <979623074@qq.com>
2017-09-05 22:43:33 +0800

    main

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,9 +1,20 @@
 app.controller('mainCtl',  function($scope,$rootScope){
+	let docHeight = $(window).height();
+	let workbench_header = $('#workbench_header').outerHeight(true);
+	$scope.headerBackground = {
+		url:'/images/main/2.png',
+		height:docHeight
+	}
     $scope.title = "main Page";	
     $scope.projectList = [
     	{title:'乡村夏令营',linkUrl:'#/',photoUrl:'/images/main/1.png',desc:'乡村夏令营'},
     	{title:'乡村医疗',linkUrl:'#/',photoUrl:'/images/main/2.png',desc:'乡村医疗'},
     	{title:'乡村创客',linkUrl:'#/',photoUrl:'/images/main/3.jpg',desc:'乡村创客'},
-    	{title:'暖冬行动',linkUrl:'#/',photoUrl:'/images/main/4.png',desc:'暖冬行动'},
+    	{title:'暖冬行动',linkUrl:'#/',photoUrl:'/images/main/4.png',desc:'暖冬行动'}
+    ];
+    $scope.partnerList = [
+    	{title:'公益合伙人',photoUrl:'/images/main/1.png'},
+    	{title:'合作机构',photoUrl:'/images/main/2.png'},
+    	{title:'合作企业',photoUrl:'/images/main/3.jpg'}
     ];
 });
