2697bb1f7b43502e59b38c98b21f17665b55ccf7
swl979623074 <979623074@qq.com>
2017-09-07 22:40:48 +0800

    'workbench/news'

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -12,6 +12,17 @@ app.controller('mainCtl',  function($scope,$rootScope){
     	{title:'乡村创客',linkUrl:'#/',photoUrl:'/images/main/3.jpg',desc:'乡村创客'},
     	{title:'暖冬行动',linkUrl:'#/',photoUrl:'/images/main/4.png',desc:'暖冬行动'}
     ];
+    $scope.newsList = [
+    	{title:'最新动态',linkUrl:'#/',photoUrl:'/images/news/1.jpg'},
+    	{title:'最新动态',linkUrl:'#/',photoUrl:'/images/news/2.jpg'},
+    	{title:'最新动态',linkUrl:'#/',photoUrl:'/images/news/3.jpg'},
+    	{title:'最新动态',linkUrl:'#/',photoUrl:'/images/news/4.jpg'},
+    	{title:'最新动态',linkUrl:'#/',photoUrl:'/images/news/5.jpg'},
+    	{title:'最新动态',linkUrl:'#/',photoUrl:'/images/news/6.jpg'},
+    	{title:'最新动态',linkUrl:'#/',photoUrl:'/images/news/7.jpg'},
+    	{title:'最新动态',linkUrl:'#/',photoUrl:'/images/news/8.jpg'},
+    	{title:'最新动态',linkUrl:'#/',photoUrl:'/images/news/9.jpg'},
+    ];
     $scope.partnerList = [
     	{title:'公益合伙人',photoUrl:'/images/main/1.png'},
     	{title:'合作机构',photoUrl:'/images/main/2.png'},
